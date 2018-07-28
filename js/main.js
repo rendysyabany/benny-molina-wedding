22b35d75e2444319e83f3a7128eea15e883995f7
Rendyansyah Syabany <rendysyabany@gmail.com>
2018-07-28 16:15:34 +0700

    add fullpage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-// Firefly
-
+// Firefly Effect
 $.firefly({
     color: '#fff',
     minPixel: 1,
     maxPixel: 4,
-    total : 70,
+    total : 100,
     on: '#firefly',
     borderRadius: 100
   });
+
+// FullPage
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['firstPage', 'secondPage', '3rdPage'],
+    sectionsColor: ['#fff', '#fff', '#fff'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['First page', 'Second page', 'Third and last page']
+});
